a55ce0e9dc937c926bcd0493a233837ff63328f4
sweta.kumari <sweta.kumari@Evalueserve.com>
2017-08-11 15:31:21 +0530

    trail third

--- gitpoc/src/test/Helloworld.java
+++ gitpoc/src/test/Helloworld.java
@@ -5,5 +5,8 @@ public class Helloworld
 public static void main(String a[])
 {
 	System.out.println("git commit");
+	filename f=new filename();
+	int z=f.sum(3, 5);
+	System.out.println(z);
 }
 }
---
+++ gitpoc/src/test/filename.java
@@ -0,0 +1,10 @@
+package test;
+
+public class filename 
+{
+public int sum(int x, int y)
+{
+	int z=x+y;
+	return z;
+}
+}
